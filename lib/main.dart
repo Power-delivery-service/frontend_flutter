983b1cfea24e509fa7e961a96ff867b997336130
kkristinakkorn <89091819+kkristinakkorn@users.noreply.github.com>
2022-08-21 23:34:52 +0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:rosseti/pages/home.dart';
+import 'package:rosseti/pages/MainPage.dart';
+import 'package:rosseti/pages/registration.dart';
+import 'package:map/map.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  theme: ThemeData(
+      primaryColor: Colors.deepOrangeAccent
+  ),
+  initialRoute: '/',
+    routes:{
+      '/':(context) => MainPage(),
+      '/home': (context) => const Home(),
+      '/registration': (context) =>  LoginScreen(),
+
+
+    },
+));
